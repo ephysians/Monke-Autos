62ef284dc21f7440b9035432f05203eac4af62b4
Emmanuel Chukwukere <chuksobinna80@gmail.com>
2024-12-01 19:57:57 +0100

    second code update

--- components/Heros/index.tsx
+++ components/Heros/index.tsx
@@ -1,46 +1,37 @@
 "use client";
 
-import React from 'react'
-import CustomButton from '../CustomButton'
-import Image from 'next/image';
-
-
+import React from "react";
+import CustomButton from "../CustomButton";
+import Image from "next/image";
 
 const Heros = () => {
-  const handleScroll = () => { };
+  const handleScroll = () => {};
 
   return (
-    <div className='hero'>
-      <div className='flex-1 pt-36 padding-x'>
-  <h1 className='hero__title'>
-  Premium SUVs, Expert Care, Unmatched Service!
-</h1>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Premium SUVs, Expert Care, Unmatched Service!
+        </h1>
 
-<p className='hero__subtitle'>
-  Luxury sales, repairs, and maintenance made simple.
-</p>
+        <p className="hero__subtitle">
+          Luxury sales, repairs, and maintenance made simple.
+        </p>
 
         <CustomButton
-  title="Explore Cars"
-  containerStyles="bg-primary-blue text-white rounded-lg mt-6 Shadow-black"
-  handleClick={handleScroll}
-/>
-
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-lg mt-6 Shadow-black"
+          handleClick={handleScroll}
+        />
       </div>
-<div className='hero__image-container'>
-  <div className='hero__image'>
-    <Image src="/hero.png" 
-     alt='hero' 
-     fill className='object-contain' 
-     />
-
-    <div  className=" hero__image-overlay"/>
-  </div>
-  
-
-</div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className=" hero__image-overlay" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Heros
+export default Heros;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -21,10 +21,11 @@ const Navbar = () => {
     <CustomButton
       title="Sign In"
       btnType="button"
-      containerStyles="text-primary-blue shadow-custom font-medium rounded-md bg-white min-w-[130px]"
+      containerStyles="text-primary-blue shadow-custom font-bold rounded-md bg-white min-w-[130px]"
     />
   </nav>
 </header>
+
 
   )
 }
