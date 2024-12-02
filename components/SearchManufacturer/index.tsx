c41f4b8025a85ac3d00b0ec0bb599a1300cb8b59
Emmanuel Chuks <104981589+ephysians@users.noreply.github.com>
2024-12-02 15:23:57 +0000

    last update at office

--- components/SearchManufacturer/index.tsx
+++ components/SearchManufacturer/index.tsx
@@ -17,7 +17,7 @@
 //                  width={20}
 //                  height={20}
 //                  className="ml-4"
-//                  alt="car-logo" 
+//                  alt="car-logo"
 //                 />
 //             </Combobox.Button>
 //         </div>
@@ -30,43 +30,70 @@
 
 "use client";
 import { SearchManufacturerProps } from "@/types";
-import { Combobox } from "@headlessui/react";
+import { CheckIcon } from '@heroicons/react/20/solid'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
 import Image from "next/image";
+import clsx from "clsx";
+import { useState, Fragment } from "react";
 
-const SearchManufacturer = ({manufacturer,
-  setManufacturer }: SearchManufacturerProps) => {
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
   return (
-    <div className="search-manufacturer">
-      <Combobox
-        value={manufacturer}
-        onChange={setManufacturer}
-      >
-        <div className="relative w-full">
-          {/* Image positioned absolutely */}
-          <div className="absolute top-[50%] left-4 transform -translate-y-1/2">
+    <div className="search-manufacturer mt-6">
+      <Combobox value={manufacturer} onChange={setManufacturer}>
+        <div className="relative w-full border top-4 ">
+          <div className="absolute top-[25px]  w-full transform -translate-y-1/2">
             <Image
               src="/car-logo.svg"
               width={20}
               height={20}
+              className="ml-4"
               alt="car-logo"
             />
           </div>
 
-          {/* Input field with padding to avoid overlap */}
-          <Combobox.Input
-            className="search-input pl-12" // Add padding to make space for the image
-            placeholder="Search manufacturer"
-            onChange={(e) => setManufacturer(e.target.value)}
+          <ComboboxInput
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
           />
         </div>
         <Combobox.Options>
           {/* Add dropdown options dynamically */}
         </Combobox.Options>
       </Combobox>
+
+      <ComboboxOptions
+        anchor="bottom"
+        transition
+        className={clsx(
+          "w-[var(--input-width)] rounded-xl border border-white/5 bg-white/5 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible",
+          "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0"
+        )}
+      >
+        {filteredPeople.map((person) => (
+          <ComboboxOption
+            key={person.id}
+            value={person}
+            className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+          >
+            <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible" />
+            <div className="text-sm/6 text-white">{person.name}</div>
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
     </div>
   );
 };
 
 export default SearchManufacturer;
-
-
