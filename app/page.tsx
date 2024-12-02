fbe9ba35e85b7410ee2a049d926e144eb60ad0a3
Emmanuel Chukwukere <chuksobinna80@gmail.com>
2024-12-02 07:15:43 +0100

    third update

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,13 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Heros />
+      
+      <div className ="mt-12 padding-x  padding-y max-width" id="discover" >
+        <div className="home__ text-container">
+            <h1 className="text-4l font-extrabold">Catalogue</h1>
+            <p> Explor the Cars you might like</p>
+        </div>
+      </div>
     </main>
   );
 }
