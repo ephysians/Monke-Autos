772a6191d83deb1350740f84f4cbfc2d58097146
ephysians <chuksobinna80@gmail.com>
2024-12-18 16:00:17 +0100

    new update

--- components/OurOffers/index.tsx
+++ components/OurOffers/index.tsx
@@ -1,30 +1,33 @@
 import { OurOfferValues } from "@/constants";
-
-
+import Image from "next/image";
 
 const Offers = () => {
   return (
-    <div className="flex overflow-x-auto gap-4 py-7 px-10 mx-20">
+    <div className="flex overflow-x-auto gap-4 py-7 px-10 mx-20 items-center justify-center h-auto">
       {OurOfferValues.map((offer, index) => (
         <div
           key={index}
-          className="flex-shrink-0 w-64 p-4 bg-white border rounded-lg shadow-md "
+          className="flex-shrink-0 p-4 bg-white border w-[286px] h-[387px] "
         >
-          <img
-            src={offer.image}
-            alt={offer.title}
-            className="w-full h-40 object-cover rounded-md mb-4"
-          />
-          <h3 className="text-lg font-bold mb-2">{offer.title}</h3>
-          <p className="text-sm text-gray-600">{offer.text}</p>
-          {offer.readmoreUrl && (
-            <a
-              href={offer.readmoreUrl}
-              className="mt-2 inline-block text-primary-blue underline"
-            >
-              Read more
-            </a>
-          )}
+          <div className="border-b-2 text-center">
+            <h3 className="text-lg font-bold mb-2 ">{offer.title}</h3>
+          </div>
+
+          <div>
+            <Image
+              src={offer.image}
+              alt={offer.title}
+              width={256}
+              height={160}
+              className=" w-full h-[181px]"
+            />
+            <p className="text-sm text-gray-600">{offer.text}</p>
+            {offer.readmoreUrl && (
+              <div className=" w-full bg-primary-blue p-5 h-[34px] text-center">
+                <a href={offer.readmoreUrl} className="text-center text-white">Read more</a>
+              </div>
+            )}
+          </div>
         </div>
       ))}
     </div>
@@ -32,3 +35,4 @@ const Offers = () => {
 };
 
 export default Offers;
+
