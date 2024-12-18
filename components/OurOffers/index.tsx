972e02734cd85bc7ce702d05ede523af26ab356c
ephysians <chuksobinna80@gmail.com>
2024-12-18 16:57:33 +0100

    new update

--- components/OurOffers/index.tsx
+++ components/OurOffers/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Offers = () => {
   return (
-    <div className="flex overflow-x-auto gap-4 py-7 px-10 mx-20 items-center justify-center h-auto">
+    <div className="flex overflow-x-auto gap-4 py-7 px-8 mx-20 items-center justify-center h-auto">
       {OurOfferValues.map((offer, index) => (
         <div
           key={index}
@@ -15,7 +15,7 @@ const Offers = () => {
 
           <div>
             <Image
-              src={`/images/${offer.image.split('/').pop()}`}  // Assuming images are in the public/images folder
+              src={`/images/${offer.image.split("/").pop()}`} // Assuming images are in the public/images folder
               alt={offer.alt1}
               width={256}
               height={160}
@@ -23,8 +23,8 @@ const Offers = () => {
             />
             <p className="text-sm text-gray-600 mb-8">{offer.text}</p>
             {offer.readmoreUrl && (
-              <div className=" w-full bg-primary-blue p-5 h-[34px] text-center">
-                <a href={offer.readmoreUrl} className="text-center text-white">
+              <div className="w-full bg-primary-blue h-[34px] border-1.5 border-yellow-400 text-white flex items-center justify-center">
+                <a href={offer.readmoreUrl} className="text-white">
                   Read more
                 </a>
               </div>
